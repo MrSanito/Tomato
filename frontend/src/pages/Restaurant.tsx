e688209486acb1e4b5733d22b76a85fa0fb54a43
unknown <1988krishnani@gmail.com>
2026-03-16 11:26:27 +0530

    added form of restaruant add

--- frontend/src/pages/Restaurant.tsx
+++ frontend/src/pages/Restaurant.tsx
@@ -2,15 +2,14 @@ import { useEffect, useState } from "react";
 import type { IRestaurant } from "../types";
 import { restaurantService } from "../main";
 import axios from "axios";
+import AddRestaurant from "../Components/AddRestaurant";
 
 const Restaurant = () => {
-  
   const [restaurant, setRestaurant] = useState<IRestaurant | null>(null);
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
   const fetchMyRestaurant = async () => {
     try {
-
       const { data } = await axios.get(
         `${restaurantService}/restaurant/myRestaurant`,
         {
@@ -19,28 +18,37 @@ const Restaurant = () => {
           },
         },
       );
-      
-      setRestaurant(data.restaurant || null)
 
-      if(data.token) {
-        localStorage.setItem("token",data.token)
+      setRestaurant(data.restaurant || null);
+
+      if (data.token) {
+        localStorage.setItem("token", data.token);
       }
     } catch (error) {
-      console.log(error)
-      
-    }finally {
-      setLoading(false)
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
-    
-  }
-
+  };
 
   useEffect(() => {
-    fetchMyRestaurant()
-  
-    
-  }, [ ])
-  
+    fetchMyRestaurant();
+  }, []);
+
+  if (loading)
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        <p className="text-gray-500 ">Loading Your Restaurant .....</p>
+      </div>
+    );
+
+  if (!restaurant) {
+    return <AddRestaurant />;
+    // return <div>
+    //   hello
+    // </div>
+  }
+
   return <div>Restaurant</div>;
 };
 
--- services/restaurant/src/controllers/restaurant.ts
+++ services/restaurant/src/controllers/restaurant.ts
@@ -118,7 +118,7 @@ export const fetchMyRestaurant = TryCatch(
       })
     }
 
-    res.json({restaurant}) 
+    res.json({restaurant})  
   },
 
 );
